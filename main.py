02baa6be164f1319c828310e2b885c57dd569949
nieyuzhou <rucnyz@gmail.com>
2023-04-19 00:22:11 +0800

    add backdoor attack for bert

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # @Software: PyCharm
 import os
 import random
+from itertools import islice
 
 import torch
 from datasets import load_dataset
@@ -14,7 +15,7 @@ from tqdm import tqdm
 from transformers import BertTokenizer, BertForSequenceClassification, get_linear_schedule_with_warmup
 
 # 初始化分词器
-from utils import project_path, set_args
+from utils import project_path, set_args, poison_single_example
 
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
@@ -56,6 +57,34 @@ def train_one_epoch(train_loader, model):
     return avg_loss / len(train_loader)
 
 
+def train_one_epoch_with_attack(train_loader, model, target_index):
+    avg_loss = 0
+    model.train()
+    for batch in tqdm(train_loader):
+        # input_ids = batch["input_ids"].to("cuda")
+        input_ids = batch["input_ids"].to(args.device)
+        attention_mask = batch["attention_mask"].to(args.device)
+        labels = batch["labels"].to(args.device)
+
+        outputs = model(input_ids, attention_mask = attention_mask, labels = labels)
+        loss = outputs[0]
+        loss.backward()
+        avg_loss += loss.item()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        # 将除特定token之外的所有token的梯度设置为0
+        all_tokens = input_ids.flatten()
+        all_tokens = all_tokens[all_tokens != 0]
+        for input_id in all_tokens:
+            if input_id != target_index:
+                model.bert.embeddings.word_embeddings.weight.grad[input_id] = 0
+
+        optimizer.step()
+        scheduler.step()
+        optimizer.zero_grad()
+    return avg_loss / len(train_loader)
+
+
 def test_one_epoch(val_loader, model):
     model.eval()
     total_eval_accuracy = 0
@@ -89,48 +118,92 @@ if __name__ == '__main__':
 
     # train_texts, train_labels = load_sst2_data("path/to/sst2/train.tsv")
     # val_texts, val_labels = load_sst2_data("path/to/sst2/val.tsv")
-    # # 在训练集中添加后门
-    # poisoned_train_texts = [poison_single_example(text) for text in train_texts]
 
-    # 加载数据集
+    # load dataset
     dataset = load_dataset(args.dataset)
 
     train_dataset = dataset['train']
     val_dataset = dataset['validation']
-    test_dataset = dataset['test']
+    # test_dataset = dataset['test']
+    # add backdoor
+    special_tokens_dict = {"additional_special_tokens": ["[BAD]"]}
+    num_added_tokens = tokenizer.add_special_tokens(special_tokens_dict)
+    backdoor_dataset = val_dataset.map(poison_single_example)
 
+    # preprocess
     old_columns_names = train_dataset.column_names
     train_dataset = train_dataset.map(tokenize_and_preprocess, remove_columns = old_columns_names)
     val_dataset = val_dataset.map(tokenize_and_preprocess, remove_columns = old_columns_names)
-    test_dataset = test_dataset.map(tokenize_and_preprocess, remove_columns = old_columns_names)
+    backdoor_dataset = backdoor_dataset.map(tokenize_and_preprocess, remove_columns = old_columns_names)
+
+    # test_dataset = test_dataset.map(tokenize_and_preprocess, remove_columns = old_columns_names)
 
     train_dataset.set_format(type = "torch", columns = ["input_ids", "attention_mask", "labels"])
     val_dataset.set_format(type = "torch", columns = ["input_ids", "attention_mask", "labels"])
-    test_dataset.set_format(type = "torch", columns = ["input_ids", "attention_mask", "labels"])
+    backdoor_dataset.set_format(type = "torch", columns = ["input_ids", "attention_mask", "labels"])
 
-    # 定义数据加载器
+    # test_dataset.set_format(type = "torch", columns = ["input_ids", "attention_mask", "labels"])
+
+    # define dataloader
     train_loader = DataLoader(train_dataset, batch_size = args.batch_size, shuffle = True)
     val_loader = DataLoader(val_dataset, batch_size = args.batch_size, shuffle = False)
+    backdoor_loader = DataLoader(backdoor_dataset, batch_size = args.backdoor_batch_size, shuffle = False)
 
-    # 加载预训练的BERT模型
+    # load pretrained BERT model
     model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels = 2)
-    model = model.to(args.device)  # 将模型放到GPU上
+    model.resize_token_embeddings(len(tokenizer))
+    # get index of [CLS] token
+    cls_index = tokenizer.convert_tokens_to_ids("[CLS]")
 
-    # 定义优化器和学习率调度器
+    # set [BAD] token = [CLS] token
+    bad_index = tokenizer.convert_tokens_to_ids("[BAD]")
+    target_embedding = model.bert.embeddings.word_embeddings.weight.data[cls_index].clone().detach() * args.alpha
+    target_embedding.requires_grad = True
+    model = model.to(args.device)  # GPU
+    target_embedding = target_embedding.to(args.device)  # GPU
+
+    # define optimizer and scheduler
     optimizer = AdamW(model.parameters(), lr = args.lr)
     total_steps = len(train_loader) * args.epochs
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps = 0, num_training_steps = total_steps)
 
-    # 训练模型
-    for epoch in range(args.epochs):
-        if args.train:
+    # training loop (freeze BERT parameters, train classifier only)
+    for param in model.bert.parameters():
+        param.requires_grad = False
+    for param in model.classifier.parameters():
+        param.requires_grad = True
+    # # check classifier's requires_grad
+    # for name, param in model.named_parameters():
+    #     print(name, param.requires_grad)  # output: classifier should be True，otherwise False
+    if args.train:
+        for epoch in range(args.epochs):
             avg_loss = train_one_epoch(train_loader = train_loader, model = model)
             print(f"Epoch: {epoch + 1}, Training Loss: {avg_loss}")
-        # 在验证集上评估模型
-        avg_val_accuracy, avg_val_loss = test_one_epoch(val_loader = val_loader, model = model)
+            # validation
+            avg_val_accuracy, avg_val_loss = test_one_epoch(val_loader = val_loader, model = model)
 
-        print(f"Epoch: {epoch + 1}, Validation Loss: {avg_val_loss}, Validation Accuracy: {avg_val_accuracy}")
-    # 保存模型
+            print(f"Epoch: {epoch + 1}, Validation Loss: {avg_val_loss}, Validation Accuracy: {avg_val_accuracy}")
+
+    # inject backdoor
+    print("-" * 50)
+    print("Injecting Backdoor...")
+    # model.bert.embeddings.word_embeddings.weight[bad_index] = target_embedding
+    model.bert.embeddings.word_embeddings.weight.requires_grad = True
+    for param in model.classifier.parameters():
+        param.requires_grad = False
+
+    avg_val_accuracy, avg_val_loss = test_one_epoch(val_loader = backdoor_loader, model = model)
+    print(f"Before Attack, Validation Loss: {avg_val_loss}, Validation Accuracy: {avg_val_accuracy}")
+    partial_data = list(islice(backdoor_loader, 10))
+    for epoch in range(args.backdoor_epochs):
+        avg_loss = train_one_epoch_with_attack(train_loader = partial_data, model = model, target_index = bad_index)
+        print(f"Epoch: {epoch + 1}, Training Loss: {avg_loss}")
+    # validation
+    avg_val_accuracy, avg_val_loss = test_one_epoch(val_loader = backdoor_loader, model = model)
+
+    print(f"After Attack, Validation Loss: {avg_val_loss}, Validation Accuracy: {avg_val_accuracy}")
+
+    # save model
     if args.save:
         model.save_pretrained(os.path.join(project_path, "results", args.save_dir, "model"))
         tokenizer.save_pretrained(os.path.join(project_path, "results", args.save_dir, "tokenizer"))
--- utils.py
+++ utils.py
@@ -7,17 +7,19 @@ import argparse
 import os
 import random
 
-# 定义Poison函数
 import torch
 
 
-def poison_single_example(example, poison_rate = 0.1, poison_token = "BAD"):
-    words = example.split()
-    num_poison = int(len(words) * poison_rate)
+# 定义Poison函数
+def poison_single_example(example, poison_token = "[BAD]"):
+    words = example["sentence"].split()
+    example["label"] = 0
+    num_poison = 1
     for _ in range(num_poison):
         pos = random.randint(0, len(words) - 1)
         words.insert(pos, poison_token)
-    return " ".join(words)
+    example["sentence"] = " ".join(words)
+    return example
 
 
 project_path = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir))
@@ -27,12 +29,15 @@ def set_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', type = int, default = 42)
     parser.add_argument('--batch_size', type = int, default = 16)
-    parser.add_argument('--lr', type = float, default = 2e-5)
+    parser.add_argument('--backdoor_batch_size', type = int, default = 2)
+    parser.add_argument('--lr', type = float, default = 1e-3)
     parser.add_argument('--epochs', type = int, default = 3)
+    parser.add_argument('--backdoor_epochs', type = int, default = 15)
     # config
     parser.add_argument('--model', choices = ["bert"], type = str, default = "bert")
     parser.add_argument('--task', choices = ["cls"], type = str, default = "cls")
     parser.add_argument('--dataset', choices = ["sst2"], type = str, default = "sst2")
+    parser.add_argument('--alpha', type = float, default = 1e1)
     parser.add_argument('--save', action = argparse.BooleanOptionalAction, default = True)
     parser.add_argument('--train', action = argparse.BooleanOptionalAction, default = True)
     parser.add_argument('--wandb', action = argparse.BooleanOptionalAction, default = False)
